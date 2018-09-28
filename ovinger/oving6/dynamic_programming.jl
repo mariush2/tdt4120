56ac10b132c2afa18ebce56177c629978f473ef2
Marius Hauge Tobiassen <mariuhto@stud.ntnu.no>
2018-09-28 10:16:57 +0200

    Done øving6

--- ovinger/oving6/dynamic_programming.jl
+++ ovinger/oving6/dynamic_programming.jl
@@ -1,69 +1,108 @@
+# All functions return 100% correct! :D
 function cumulative(weights)
     #Select using wieght[x, y]
-    path_weights = zeros(Int64, 5, 5)
-    len = 5
-    for i = 1:len
+    weights_x, weights_y = size(weights)
+    path_weights = zeros(Int64, weights_x, weights_y)
+
+    for i = 1:weights_y
         path_weights[1, i] = weights[1, i]
     end
-    for x = 1:len - 1
-        for y = 1:len
+
+    for x = 1:weights_x - 1
+        for y = 1:weights_y
             current = weights[x + 1, y]
-            if y != 1 && y != len
+            chosen = 0
+            if y != 1 && y != weights_y
                 #check both sides
-                if weights[x, y - 1] + current < weights[x, y] + current
-                    #left
-                    path_weights[x + 1, y] = weights[x, y - 1] + current
-                elseif weights[x, y] + current < weights[x, y + 1] + current
-                    #down
-                    path_weights[x + 1, y] = weights[x, y] + current
+                if weights[x, y - 1] <= weights[x, y] && weights[x, y - 1] <= weights[x, y + 1]
+                    # 100% the smallest to the left
+                    chosen = weights[x, y - 1]
+                elseif weights[x, y] <= weights[x, y - 1] && weights[x, y] <= weights[x, y + 1]
+                    chosen = weights[x, y]
                 else
-                    #right
-                    path_weights[x + 1, y] = weights[x, y + 1] + current
+                    chosen = weights[x, y + 1]
                 end
             elseif y == 1
-                #check only right
-                if weights[x, y + 1] + current < weights[x, y] + current
-                    path_weights[x + 1, y] = weights[x, y + 1] + current
+                #Check only right
+                if weights[x, y] <= weights[x, y + 1]
+                    chosen = weights[x, y]
                 else
-                    path_weights[x + 1, y] = weights[x, y] + current
+                    chosen = weights[x, y + 1]
                 end
             else
-                #check only left
-                if weights[x, y - 1] + current < weights[x, y] + current
-                    path_weights[x + 1, y] = weights[x, y - 1] + current
+                #Check only left
+                if weights[x, y - 1] <= weights[x, y]
+                    chosen = weights[x, y - 1]
                 else
-                    path_weights[x + 1, y] = weights[x, y] + current
+                    chosen = weights[x, y]
                 end
             end
+            path_weights[x + 1, y] = chosen + current
             weights[x + 1, y] = path_weights[x + 1, y]
         end
     end
     return path_weights
 end
 
-function test_cumulative()
-    weights = [3  6  8 6 3;
-           7  6  5 7 3;
-           4  10 4 1 6;
-           10 4  3 1 2;
-           6  1  7 3 9]
+function back_track(path_weights)
+    path = []
+    x, y = size(path_weights)
+    #Init last value
+    last_y = 0
+    last_val = Inf
+    for k = 1:y
+        if path_weights[x, k] < last_val
+            last_y = k
+            last_val = path_weights[x, k]
+        end
+    end
+    last = (x, last_y)
+    push!(path, last)
+    for i = x - 1:-1:1
+        current = Inf
+        current_y = 0
+        if last_y != 1 && last_y != y
+            #Check all sides
+            if path_weights[i, last_y - 1] < current
+               current = path_weights[i, last_y - 1]
+               current_y = last_y - 1
+            end
 
-    path_weights = [3  6  8  6  3;
-                10 9  11 10 6;
-                13 19 13 7  12;
-                23 17 10 8  9;
-                23 11 15 11 17]
-    println("Path_weights:", typeof(path_weights))
-    for x = 1:5
-        println(path_weights[x, 1:5])
+            if path_weights[i, last_y] < current
+                current = path_weights[i, last[2]]
+                current_y = last_y
+            end
+
+            if path_weights[i, last_y + 1] < current
+                current = path_weights[i, last_y]
+                current_y = last_y + 1
+            end
+        elseif last_y == 1
+            #Check only right
+            if path_weights[i, last_y] < current
+                current = path_weights[i, last_y]
+                current_y = last_y
+            end
+
+            if path_weights[i, last_y + 1] < current
+                current = path_weights[i, last_y + 1]
+                current_y = last_y + 1
+            end
+        else
+            if path_weights[i, last_y - 1] < current
+               current = path_weights[i, last_y - 1]
+               current_y = last_y - 1
+            end
+
+            if path_weights[i, last_y] < current
+                current = path_weights[i, last_y]
+                current_y = last_y
+            end
+        end
+        chosen = (i, current_y)
+        last = chosen
+        last_y = current_y
+        push!(path, chosen)
     end
-    println("\n")
-    test = cumulative(weights)
-    println("Test weights:", typeof(test))
-    for x = 1:5
-        println(test[x, 1:5])
-    end
-    return test == path_weights
+    return path
 end
-
-println(test_cumulative())
