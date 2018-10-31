dc0f5063cabd9cf62a7862e8ee56cc194db869a4
Marius Hauge Tobiassen <mariuhto@stud.ntnu.no>
2018-10-31 17:41:52 +0100

    More work on øving 11

--- ovinger/oving11/alltoall.jl
+++ ovinger/oving11/alltoall.jl
@@ -1,6 +1,5 @@
 #f = min
 #g = +
-
 function f(x, y)
     return min(x, y)
 end
@@ -9,12 +8,38 @@ function g(x, y)
     return x + y
 end
 
-function floyd_warshall(adjacency_matrix, nodes) ## Remember to "add" f and g afterwards
-
+function floyd_warshall(adjacency_matrix, nodes, f::Function, g::Function)
+    D = []
+    push!(D, adjacency_matrix) # D^0 = W
+    for k = 1:nodes
+        push!(D, fill(Inf, (nodes, nodes)))
+    end
+    println("Before run")
+    print_floyd(D)
+    println("\n")
+    for k = 2:nodes
+        for i = 1:nodes
+            for j = 1:nodes
+                D[k][j, i] = f(D[k - 1][j, i], g(D[k - 1][k, i], D[k - 1][j, k]))
+            end
+        end
+    end
+    println("After run")
+    print_floyd(D)
+    return D[nodes + 1]
 end
 
 function test_floyd()
     a = [0 7 2; Inf 0 Inf; Inf 4 0]
     n = 3
-    return [0 6 2; Inf 0 Inf; Inf 4 0] == floyd_warshall(a, n)
+    return [0 6 2; Inf 0 Inf; Inf 4 0] == floyd_warshall(a, n, f::Function, g::Function)
 end
+
+function print_floyd(D)
+    for matrix in D
+        display(matrix)
+        print("\n\n")
+    end
+end
+
+println(test_floyd())
